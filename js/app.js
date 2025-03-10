8989cba7563cca00caa3e6d23f561b399916dffd
pharelle123 <zeffelesra@gmail.com>
2025-03-10 17:25:12 +0100

    app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("chargé");
+  console.log("chargé");
+
+  const elTitre = document.querySelectorAll("titre, h1");
+  elTitre.forEach(el => el.innerText = "Développeur PHP");
 });
+// const table=["mama","papa"]
+// console.table(typeof table)
+// let chiffre = 9
+// chiffre%2 == 0 ? console.log("pair") : console.log("impair")
